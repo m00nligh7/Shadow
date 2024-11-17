632e2f2487432e658157c2e3920e39d0a5c82b93
MoonLight <122781126+m00nligh7@users.noreply.github.com>
2024-11-17 15:12:01 +0300

    Fixed login screen, hiding extra windows | V: 0.3
    So i fixed login screen now and it looks good. When login window is opened, main window is hidden, when we login into our account and clicking the login button, login window is hidden and main window become visible. Yahoo UwU

--- Client.py
+++ Client.py
@@ -19,25 +19,34 @@ def send_msgs(client_socket):
         
 
 def get_nick_entry():
-    global nick_entry, client_socket
+    global nick_entry, client_socket, loginwindow
     nick_entry_result = str(nick_entry.get())
     nickname = nick_entry_result
     client_socket.send(nickname.encode('utf-8'))
+    app.deiconify()
+    loginwindow.withdraw()
+
+
+def loginwindow():
+    global nick_entry, loginwindow
+    loginwindow = customtkinter.CTk()
+    loginwindow.title("Логин")
+    loginwindow.geometry("250x200")
+    loginwindow.resizable(False, False)
+    app.withdraw()
+    nick_entry = customtkinter.CTkEntry(master=loginwindow, placeholder_text="Введите ваш никнейм", )
+    nick_entry.pack(fill = customtkinter.BOTH, padx=10, pady=(10, 5))
+    nick_button = customtkinter.CTkButton(master=loginwindow, width=60, height=32, border_width=0, corner_radius=8, text="Войти", command=get_nick_entry)
+    nick_button.pack(fill = customtkinter.BOTH, padx=10, pady=(5, 10))
 
 def main():
-    global nick_entry, client_socket
+    global nick_entry, client_socket, app
     app = customtkinter.CTk()
     app.geometry("600x500")
     app.title("ShadowTalk")
-
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 8081))
-
-    nick_entry = customtkinter.CTkEntry(master=app, placeholder_text="Введите ваш никнейм")
-    nick_entry.pack(anchor=customtkinter.CENTER)
-    nick_button = customtkinter.CTkButton(master=app, width=60, height=32, border_width=0, corner_radius=8, text="Войти", command=get_nick_entry)
-    nick_button.place(anchor=customtkinter.W)
-
+    loginwindow()
     client_hndlr_recv = threading.Thread(target=receive_msgs, args=(client_socket,))
     client_hndlr_recv.start()
     client_hndlr_send = threading.Thread(target=send_msgs, args=(client_socket,))
