7b79f7e2c21afac9ffd08e5188f032837219606b
MoonLight <122781126+m00nligh7@users.noreply.github.com>
2025-02-07 11:52:10 +0300

    Restructured code| V: 0.3
    I restructured the code to make it more understandable for others. ;)

--- Client.py
+++ Client.py
@@ -2,6 +2,32 @@ import socket
 import threading
 import customtkinter
 
+def get_nickname_ui():
+    global nick_entry, client_socket
+    nickname = str(nick_entry.get())
+    client_socket.send(nickname.encode('utf-8'))
+    main.deiconify()
+    login.withdraw()
+
+def login_ui():
+    global login, nick_entry
+    login = customtkinter.CTk()
+    login.title("Login")
+    login.geometry("250x200")
+    login.resizable(False, False)
+    nick_entry = customtkinter.CTkEntry(master=login, placeholder_text="Введите ваш никнейм", )
+    nick_entry.pack(fill = customtkinter.BOTH, padx=10, pady=(10, 5))
+    nick_button = customtkinter.CTkButton(master=login, width=60, height=32, border_width=0, corner_radius=8, text="Войти", command=get_nickname_ui)
+    nick_button.pack(fill = customtkinter.BOTH, padx=10, pady=(5, 10))
+    main.withdraw()
+
+def main_ui():
+    global main, login
+    main = customtkinter.CTk()
+    main.geometry("600x500")
+    main.title("ShadowTalk")
+    
+
 def receive_msgs(client_socket):
     while True:
         try:
@@ -16,42 +42,19 @@ def send_msgs(client_socket):
     while True:
         message = input("")
         client_socket.send(message.encode('utf-8'))
-        
-
-def get_nick_entry():
-    global nick_entry, client_socket, loginwindow
-    nick_entry_result = str(nick_entry.get())
-    nickname = nick_entry_result
-    client_socket.send(nickname.encode('utf-8'))
-    app.deiconify()
-    loginwindow.withdraw()
-
-
-def loginwindow():
-    global nick_entry, loginwindow
-    loginwindow = customtkinter.CTk()
-    loginwindow.title("Логин")
-    loginwindow.geometry("250x200")
-    loginwindow.resizable(False, False)
-    app.withdraw()
-    nick_entry = customtkinter.CTkEntry(master=loginwindow, placeholder_text="Введите ваш никнейм", )
-    nick_entry.pack(fill = customtkinter.BOTH, padx=10, pady=(10, 5))
-    nick_button = customtkinter.CTkButton(master=loginwindow, width=60, height=32, border_width=0, corner_radius=8, text="Войти", command=get_nick_entry)
-    nick_button.pack(fill = customtkinter.BOTH, padx=10, pady=(5, 10))
 
 def main():
-    global nick_entry, client_socket, app
-    app = customtkinter.CTk()
-    app.geometry("600x500")
-    app.title("ShadowTalk")
+    global client_socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect(('localhost', 8081))
-    loginwindow()
+    main_ui()
+    login_ui()
     client_hndlr_recv = threading.Thread(target=receive_msgs, args=(client_socket,))
     client_hndlr_recv.start()
     client_hndlr_send = threading.Thread(target=send_msgs, args=(client_socket,))
     client_hndlr_send.start()
-    app.mainloop()
+    main.mainloop()
+
 
 if __name__ == "__main__":
     main()
