ae73baf9803334c288bed458086a23fd710b750e
MoonLight <122781126+m00nligh7@users.noreply.github.com>
2025-03-22 21:36:53 +0300

    Emoji update
    Added a menu emoji, now you can use most of popular emojis in Shadow :D (theres some loading at start of app, dont worry, app is loading the emojis)

--- Client.py
+++ Client.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import customtkinter
+from PIL import Image, ImageTk
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
@@ -17,7 +18,8 @@ ip = None
 port = None
 selected_client = None
 current_client_list = []
-client_list_frame = None 
+client_list_frame = None
+hidden_emojiframe = True 
 
 message_queue = queue.Queue()
 
@@ -105,12 +107,214 @@ def get_nickname_ui():
             print("Warning is already visible")
     else:
         ui_nickname = nickname
+        try:
+            port = int(port)
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((ip, port))
+            private_key, public_key = generate_rsa_keys()
+            server_public_key_bytes = client_socket.recv(1024)
+            server_public_key = deserialize_public_key(server_public_key_bytes)
+            client_socket.send(serialize_public_key(public_key))
+            encrypted_aes_key = client_socket.recv(1024)
+            aes_key = decrypt_rsa(encrypted_aes_key, private_key)
+            encrypted_nickname = encrypt_aes(nickname, aes_key)
+            print("ENcRYPTED_NIcK" + str(encrypted_nickname))
+            client_socket.send(encrypted_nickname)
+            main.deiconify()
+            login.withdraw()
+            client_hndlr_recv = threading.Thread(target=receive_msgs, args=(client_socket,), daemon=True)
+            client_hndlr_recv.start()
+            main.after(100, process_queue)
+        except ValueError:
+            print("ERROR: Port is not int")
+        except socket.error as e:
+            print("ERROR: cant connect")
 
-        encrypted_nickname = encrypt_aes(nickname, aes_key)
-        print("ENcRYPTED_NIcK" + str(encrypted_nickname))
-        client_socket.send(encrypted_nickname)
-        main.deiconify()
-        login.withdraw()
+def emoji_frame():
+    global hidden_emojiframe
+    if hidden_emojiframe == True:
+        sidebar_frame_emoji.grid(row=0, column=3, sticky="ns", padx=5, pady=5)
+        hidden_emojiframe = False
+    else:
+        sidebar_frame_emoji.grid_forget()
+        hidden_emojiframe = True
+
+def load_emojis():
+    global sidebar_frame_emoji, chat_entry
+    for widget in sidebar_frame_emoji.winfo_children():
+        widget.destroy()
+
+    emoji_categories = {
+        "Smileys": ["😀", "😃", "😄", "😁", "😆", "😅",
+                    "😂", "🤣", "😊", "😇", "🥰", "😍",
+                    "🤩", "😘", "😗", "😚", "😙", "😋",
+                    "😛", "😜", "🤪", "😝", "🤑", "🤗",
+                    "🤭", "🤫", "🤔", "😐", "😑", "😶",
+                    "😏", "😒", "🙄", "😬", "🤥", "😌",
+                    "😔", "😪", "🤤", "😴", "😷", "🤒",
+                    "🤕", "🤢", "🤮", "🤧", "🥵", "🥶",
+                    "🥴", "😵", "🤯", "🤠", "🥳", "😎",
+                    "🤓", "🧐", "😕", "😟", "🙁", "☹️",
+                    "😮", "😯", "😲", "😳", "🥺", "😦",
+                    "😧", "😨", "😰", "😥", "😢", "😭",
+                    "😱", "😖", "😣", "😞", "😓", "😩",
+                    "😫", "🥱", "😤", "😡", "😠", "🤬",
+                    "😈", "👿", "💀", "☠️", "💩", "🤡",
+                    "👹", "👺", "👻"],
+        "Nature": ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊",
+                "🐻", "🐼", "🐨", "🐯", "🦁", "🐮",
+                "🐷", "🐽", "🐸", "🐵", "🙈", "🙉",
+                "🙊", "🐒", "🐔", "🐧", "🐦", "🐤",
+                "🐣", "🐥", "🦆", "🦅", "🦉", "🦇",
+                "🐺", "🐗", "🐴", "🦄", "🐝", "🐛",
+                "🦋", "🐌", "🐞", "🐜", "🦟", "🦗",
+                "🕷️", "🕸️", "🦂", "🐢", "🐍", "🦎",
+                "🦖", "🦕", "🐙", "🦑", "🦐", "🦞",
+                "🦀", "🐡", "🐠", "🐟", "🐬", "🐳",
+                "🐋", "🦈", "🐊", "🐅", "🐆", "🦓",
+                "🦍", "🦧", "🦣", "🐘", "🦛", "🦏",
+                "🦒", "🦘", "🦬", "🐃", "🐂", "🐄",
+                "🐎", "🐖", "🐑", "🦙", "🐐", "🦌",
+                "🦔", "🐿️", "🦫", "🦡", "🦤", "🦥",
+                "🦦", "🦨", "🦘", "🦡", "🐾"],
+        "Food": ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌",
+                "🍉", "🍇", "🍓", "🫐", "🍈", "🍒",
+                "🍑", "🥭", "🍍", "🥥", "🥝", "🍅",
+                "🍆", "🥑", "🥦", "🥬", "🥒", "🌶️",
+                "🫑", "🌽", "🥕", "🫒", "🧄", "🧅",
+                "🥔", "🍠", "🥐", "🥯", "🍞", "🥖",
+                "🥨", "🧀", "🥚", "🍳", "🧈", "🥞",
+                "🧇", "🥓", "🥩", "🍗", "🍖", "🦴",
+                "🌭", "🍔", "🍟", "🍕", "🫓", "🥪",
+                "🥙", "🧆", "🌮", "🌯", "🫔", "🥗",
+                "🥘", "🫕", "🥫", "🍝", "🍜", "🍲",
+                "🍛", "🍣", "🍱", "🥟", "🦪", "🍤",
+                "🍙", "🍚", "🍘", "🍥", "🥠", "🥮",
+                "🍢", "🍡", "🍧", "🍨", "🍦", "🥧",
+                "🧁", "🍰", "🎂", "🍮", "🍭", "🍬",
+                "🍫", "🍿", "🍩", "🍪", "🌰", "🥜",
+                "🍯", "🥛", "🍼", "🫖", "☕️", "🍵",
+                "🧃", "🥤", "🍷", "🥃", "🍸", "🍹",
+                "🧉", "🧊"],
+        "Activities": ["⚽️", "🏀", "🏈", "⚾️", "🥎", "🎾",
+                    "🏐", "🏉", "🥏", "🎱", "🪀", "🏓",
+                    "🏸", "🏒", "🏑", "🥍", "🏏", "🪃",
+                    "🥅", "⛳️", "🏹", "🎣", "🤿", "🥊",
+                    "🥋", "🎽", "🛹", "🛼", "🛷", "⛸️",
+                    "🥌", "🎿", "⛷️", "🏂", "🪂", "🏋️‍♀️",
+                    "🏋️", "🏋️‍♂️", "🤼‍♀️", "🤼", "🤼‍♂️", "🤸‍♀️",
+                    "🤸", "🤸‍♂️", "⛹️‍♀️", "⛹️", "⛹️‍♂️", "🤺",
+                    "🤾‍♀️", "🤾", "🤾‍♂️", "🏌️‍♀️", "🏌️", "🏌️‍♂️",
+                    "🏇", "🧘‍♀️", "🧘", "🧘‍♂️", "🏄‍♀️", "🏄",
+                    "🏄‍♂️", "🏊‍♀️", "🏊", "🏊‍♂️", "🤽‍♀️", "🤽",
+                    "🤽‍♂️", "🚣‍♀️", "🚣", "🚣‍♂️", "🧗‍♀️", "🧗",
+                    "🧗‍♂️", "🚵‍♀️", "🚵", "🚵‍♂️", "🚴‍♀️", "🚴",
+                    "🚴‍♂️", "🏆", "🥇", "🥈", "🥉", "🏅",
+                    "🎖️", "🏵️", "🎗️", "🎫", "🎟️", "🎪",
+                    "🤹‍♀️", "🤹", "🤹‍♂️", "🎭", "🩰", "🎨",
+                    "🎬", "🎤", "🎧", "🎼", "🎹", "🥁",
+                    "🪘", "🎷", "🎺", "🎸", "🪕", "🎻",
+                    "🎲", "♟️", "🧩", "🎯", "🎳", "🎮"],
+        "Travel": ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️",
+                "🚓", "🚑", "🚒", "🚐", "🚚", "🚛",
+                "🚜", "🦯", "🦽", "🦼", "🛴", "🚲",
+                "🛵", "🏍️", "🛺", "🚨", "🚔", "🚍",
+                "🚘", "🚖", "🚡", "🚠", "🚟", "🚃",
+                "🚋", "🚞", "🚝", "🚄", "🚅", "🚈",
+                "🚂", "🚆", "🚇", "🚊", "🚉", "✈️",
+                "🛫", "🛬", "🛩️", "💺", "🛰️", "🚀",
+                "🛸", "🚁", "🛶", "⛵️", "🚤", "🛥️",
+                "🛳️", "⛴️", "🚢", "⚓️", "🛟", "🪝",
+                "🚧", "🚦", "🚥", "🚏", "🗺️", "🗿",
+                "🗽", "🗼", "🏰", "🏯", "🏟️", "🎡",
+                "🎢", "🎠", "🏖️", "🏝️", "🏜️", "🌋",
+                "🗻", "🏕️", "🛖", "🏠", "🏡", "🏘️",
+                "🏚️", "🏗️", "🏭", "🏢", "🏬", "🏣",
+                "🏤", "🏥", "🏦", "🏨", "🏪", "🏫",
+                "🏩", "💒", "🏛️", "🗾", "🎑", "🏞️",
+                "🌅", "🌄", "🌠", "🎇", "🎆", "🌇",
+                "🌆", "🏙️", "🌃", "🌌", "🌉", "🌁"],
+        "Objects": ["⌚️", "📱", "📲", "💻", "⌨️", "🖥️",
+                "🖨️", "🖱️", "🖲️", "🕹️", "🗜️", "💽",
+                "💾", "💿", "📀", "📼", "📷", "📸",
+                "📹", "🎥", "📽️", "🎞️", "📞", "☎️",
+                "📟", "📠", "📺", "📻", "🎙️", "🎚️",
+                "🎛️", "🧭", "⏱️", "⏲️", "⏰", "🕰️",
+                "⌛️", "⏳", "📡", "🔋", "🔌", "💡",
+                "🔦", "🕯️", "🪔", "🧯", "🛢️", "💸",
+                "💵", "💴", "💶", "💷", "💰", "💳",
+                "🪙", "💎", "⚖️", "🪜", "🧰", "🪛",
+                "🔧", "🔨", "⚒️", "🛠️", "⛏️", "🪚",
+                "🔩", "⚙️", "🪤", "🧱", "⛓️", "🪝",
+                "🧲", "🔫", "💣", "🧨", "🪓", "🔪",
+                "🗡️", "⚔️", "🛡️", "🚬", "⚰️", "🪦",
+                "⚱️", "🏺", "📿", "🧸", "💈", "🪪",
+                "🏧", "🚮", "🚰", "♿️", "🚹", "🚺", "🚻", "🚼", "🚾",
+                "🛂", "🛃", "🛄", "🛅", "🚸", "⚠️",
+                "🚫", "🔞", "☢️", "☣️", "⬆️", "↗️",
+                "➡️", "↘️", "⬇️", "↙️", "⬅️", "↖️",
+                "↕️", "↔️", "↩️", "↪️", "⤴️", "⤵️",
+                "🔃", "🔄", "🔙", "🔚", "🔛", "🔜",
+                "🔝", "🔀", "🔁", "🔂", "▶️", "⏩",
+                "⏭️", "⏯️", "◀️", "⏪", "⏮️", "🔼",
+                "⏫", "🔽", "⏬", "⏸️", "⏹️", "⏺️",
+                "🎦", "🔅", "🔆", "📶", "📳", "📴",
+                "♀️", "♂️", "⚕️", "♾️", "✖️", "➕",
+                "➖", "➗", "🟰", "♾️", "‼️", "⁉️",
+                "❓", "❔", "❕", "❗️", "〰️", "💱",
+                "💲", "⚕️", "♻️", "⚜️", "🔱", "📛",
+                "🔰", "⭕️", "✅", "☑️", "✔️", "❌",
+                "❎", "➰", "➿", "〽️", "✳️", "✴️",
+                "❇️", "©️", "®️", "™️", "#️⃣", "*️⃣",
+                "0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣",
+                "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟", "🔠",
+                "🔡", "🔢", "🔣", "🔤", "🅰️", "🆎",
+                "🅱️", "🆑", "🆒", "🆓", "ℹ️", "🆔",
+                "Ⓜ️", "🆕", "🆖", "🆗", "🆘", "🆙",
+                "🆚", "🈁", "🈂️", "🈷️", "🈶", "🈯️",
+                "🉐", "🈹", "🈚️", "🈲", "🉑", "🈸",
+                "🈴", "🈳", "㊗️", "㊙️", "🈺", "🈵",
+                "🔴", "🟠", "🟡", "🟢", "🔵", "🟣",
+                "🟤", "⚫️", "⚪️", "🟥", "🟧", "🟨",
+                "🟩", "🟦", "🟪", "🟫", "⬛️", "⬜️",
+                "◼️", "◻️", "◾️", "◽️", "▪️", "▫️", "🔶",
+                "🔷", "🔸", "🔹", "🔺", "🔻", "💠",
+                "🔘", "🔳", "🔲"],
+    }
+
+    for category, emojis in emoji_categories.items():
+        category_label = customtkinter.CTkLabel(sidebar_frame_emoji, text=category, font=("Arial", 12, "bold"))
+        category_label.pack(fill = customtkinter.BOTH, padx = 5, pady = (10, 5))
+
+        row = 1
+        col = 0
+
+        emoji_frame = customtkinter.CTkFrame(sidebar_frame_emoji, fg_color="transparent")
+        emoji_frame.pack(fill=customtkinter.BOTH, padx=5, pady=5)
+
+        for emoji in emojis:
+            emoji_button = customtkinter.CTkButton(
+                emoji_frame,
+                text = emoji,
+                font = ("Arial", 16),
+                width = 30,
+                height = 30,
+                fg_color = "transparent",
+                hover_color = "gray",
+                command = lambda e=emoji: insert_emoji(e),
+            )
+            emoji_button.grid(row=row, column=col, padx=2, pady=2)
+
+            col += 1
+            if col >= 6:
+                col = 0
+                row += 1
+
+        row += 1
+
+def insert_emoji(emoji):
+    global chat_entry
+    chat_entry.insert("end", emoji)
 
 def login_ui():
     global login, nick_entry, ip_entry, port_entry
@@ -127,20 +331,23 @@ def login_ui():
     nick_button = customtkinter.CTkButton(master=login, width=60, height=32, border_width=0, corner_radius=8, text="Join", command=get_nickname_ui)
     nick_button.pack(fill=customtkinter.BOTH, padx=10, pady=(5, 10))
     login.bind('<Return>', (lambda event: get_nickname_ui()))
-    main.withdraw()
+    
 
 def main_ui():
-    global main, login, chat_entry, client_socket, txt, client_list_frame
+    global main, login, chat_entry, client_socket, txt, client_list_frame, sidebar_frame_emoji
     main = customtkinter.CTk()
-    #main.geometry("800x500")
-    main.title("ShadowTalk")
+    main.withdraw()
+    #main.geometry("830x540")
+    main.title("Shadow")
     main.resizable(False, False)
-    sidebar_frame = customtkinter.CTkFrame(main, width=200, height=470)
+    sidebar_frame = customtkinter.CTkFrame(main, width=180, height=470)
     sidebar_frame.grid(row=0, column=0, sticky="ns", padx=5, pady=5)
-    general_chat_button = customtkinter.CTkButton(sidebar_frame, text="General", command=lambda: select_client(None))
+    general_chat_button = customtkinter.CTkButton(sidebar_frame, text="General", command=lambda: select_client(None), width = 180)
     general_chat_button.pack(fill=customtkinter.BOTH, padx=5, pady=5)
-    client_list_frame = customtkinter.CTkFrame(sidebar_frame, width=190, height=400)
+    client_list_frame = customtkinter.CTkFrame(sidebar_frame, width=180, height=400, fg_color="transparent")
     client_list_frame.pack(fill=customtkinter.BOTH, padx=5, pady=5)
+    sidebar_frame_emoji = customtkinter.CTkScrollableFrame(main, width=250, height=450)
+    load_emojis()
     txt = customtkinter.CTkTextbox(main, width=600, height=470)
     txt.configure(state="disabled")
     txt.grid(row=0, column=1, columnspan=2, padx=5, pady=5)
@@ -149,6 +356,36 @@ def main_ui():
     main.bind('<Return>', (lambda event: send_msgs(client_socket)))
     send_button = customtkinter.CTkButton(main, text="Send", command=lambda: send_msgs(client_socket), width=50, height=30)
     send_button.grid(row=1, column=2, padx=5, pady=5)
+    bottom_left_frame = customtkinter.CTkFrame(main, width = 200, height = 50, fg_color="transparent")
+    bottom_left_frame.grid(row = 1, column = 0, sticky = "sw", padx = 5, pady = 5)
+    emojipng = Image.open("icons/emoji.png")
+    emojipng = emojipng.resize((28, 28))
+    icon_emoji = ImageTk.PhotoImage(emojipng)
+    filepng = Image.open("icons/file.png")
+    filepng = filepng.resize((28, 28))
+    icon_file = ImageTk.PhotoImage(filepng)
+    emoji_button = customtkinter.CTkButton(
+        bottom_left_frame,
+        image = icon_emoji,
+        text = "",
+        width = 28,
+        height = 28,
+        fg_color = "transparent",
+        #hover_color = "gray",
+        command = lambda: emoji_frame() 
+    )
+    emoji_button.grid(row = 0, column = 0, padx = 5, pady = 5)
+    file_button = customtkinter.CTkButton(
+        bottom_left_frame,
+        image = icon_file,
+        text = "",
+        width = 28,
+        height = 28,
+        fg_color = "transparent",
+        #hover_color = "gray",
+        command = lambda: print("Button 2 is pressed")
+    )
+    file_button.grid(row = 0, column = 1, padx = 5, pady = 5)
 
 def update_client_list(new_client_list):
     global client_list_frame, current_client_list
@@ -171,7 +408,7 @@ def select_client(client):
     selected_client = client
     txt.configure(state="normal")
     if client:
-        txt.insert("end", f" Chat with {client} selected\n")
+        txt.insert("end", f"Chat with {client} selected\n")
     else:
         txt.insert("end", "General chat selected\n")
     txt.configure(state="disabled")
@@ -259,21 +496,11 @@ def on_closing():
 def main():
     global client_socket, private_key, server_public_key, aes_key
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 8081))
-
-    private_key, public_key = generate_rsa_keys()
-    server_public_key_bytes = client_socket.recv(1024)
-    server_public_key = deserialize_public_key(server_public_key_bytes)
-    client_socket.send(serialize_public_key(public_key))
-
-    encrypted_aes_key = client_socket.recv(1024)
-    aes_key = decrypt_rsa(encrypted_aes_key, private_key)
+    
 
     main_ui()
     login_ui()
-    client_hndlr_recv = threading.Thread(target=receive_msgs, args=(client_socket,), daemon=True)
-    client_hndlr_recv.start()
-    main.after(100, process_queue)
+    
     main.protocol("WM_DELETE_WINDOW", on_closing)
     login.protocol("WM_DELETE_WINDOW", on_closing)
     main.mainloop()
